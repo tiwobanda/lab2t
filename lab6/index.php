83ccc2b5d0bfaa1f8e3c59cfaeadd03062d11098
Tiwonge <t.banda@rgu.ac.uk>
2019-02-14 18:01:50 +0000

    various

---
+++ lab6/index.php
@@ -0,0 +1,44 @@
+
+<html lang="en">
+<head>
+    <title>My PHP Page</title>
+</head>
+<body>
+
+<?php
+echo "Hello World";
+echo "Hello, world!";
+echo "Hello," . " " . "world" . "!";
+echo 5 * 7;
+$myname = "Fredo Baggins";
+$myage = 111;
+echo "Mayname is" . $myname . "and I am" . $myage;
+?>
+
+<?php
+echo "I get printed!";
+//I dont know! I am a coment.
+
+/*I dont get printed either
+and neither do I */
+?>
+
+<?php
+$name = "Edgar";
+
+if ($name == "Simon") {
+    print "I know you!";
+}
+else {
+    print "Who are you!";
+}
+?>
+
+<!-- Specs, Mugs, and Sausages Rolls -->
+<?php
+$my_age =
+?>
+
+
+</body>
+</html>
