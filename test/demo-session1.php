fb5beadebb6260cfacde75cb53e289712e9dfa48
tiwobanda <t.banda@rgu.ac.uk>
2019-02-19 10:42:22 +0000

    initial commit

---
+++ test/demo-session1.php
@@ -0,0 +1,31 @@
+<?php
+//start session
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Demo Session 1</title>
+</head>
+<body>
+
+<?php
+//set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+?>
+
+
+<header>
+    <h2>Welcome to your home page</h2>
+</header>
+<main>
+    <p>Hi ... you have successfully logged in</p>
+</main>
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Wedding Planit</p>
+</footer>
+</body>
+</html>
