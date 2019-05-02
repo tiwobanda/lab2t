da4095c5148e54f6eef554de029cd85df9198751
Tiwonge Banda <t.banda@rgu.ac.uk>
2019-05-02 11:48:18 +0100

    header

--- lab8/index.php
+++ lab8/index.php
@@ -3,25 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <title>Movies</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <header>
-
-</header>
-<main>
-<h2>Movies</h2>
+    <h1>Movies</h1>
+    <nav>
     <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="all-movies.php">All Movies</a></li>
         <li><a href="marvel-movies.php">Marvel Studios Movies</a></li>
         <li><a href="after2010.php">Movies after 2010</a></li>
         <li><a href="xmen-films.php">X-men Firms</a></li>
+        <li><a href=#>Others</a></li>
 
     </ul>
-</main>
+    </nav>
+
+</header>
+
+<main>
+
+
+
 <p>Welcome to our movies collection. Here we show you good movies. Click on the links above to start your search.</p>
+    <p>Let's try another list here</p>
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>
+</main>
 <footer>
-<p><p>&copy; <?php echo date("Y"); ?>. Movies.me</p>
+<p>&copy; <?php echo date("Y"); ?>. Movies.me</p>
 </footer>
 </body>
 </html>
